43fc1ab897618196680a664335d24bd72f4c5b5a
Jonas Probst <95jonas.p@gmail.com>
2023-08-07 12:32:05 +0200

    Aggregated llm_ranking results

--- indexing/querywise_indexing_T5.py
+++ indexing/querywise_indexing_T5.py
@@ -28,7 +28,7 @@ def yield_passages_from_df(df):
     for index, row in df.iterrows():
         yield {'docno': row['docno'], 'text': row['text']}
 
-def main(qid, model_name, llm_answers):
+def main(qid, model_name, llm_answers, prompt, answer_number):
     topics = load_topics("../data/topics/topics.txt", clean_queries=False)
     qrels = pt.io.read_qrels("../data/assessments/qrels.txt")
     qcred = pt.io.read_qrels("../data/assessments/qcredibility.txt")
@@ -47,24 +47,31 @@ def main(qid, model_name, llm_answers):
     passages_for_query['query'] = query
     num_docs = passages_for_query.shape[0]
 
+    output_path = './results/' + model_name + f'/{llm_answers}/{prompt}/{str(answer_number)}'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    if os.path.exists(output_path + '/query_' + qid + '_scores.csv'):
+        print('Already ranked query ' + qid + ' for model ' + model_name + ' with llm_answers ' + llm_answers + ' and prompt ' + prompt + ' and answer number ' + str(answer_number))
+        return
+
+
     if llm_answers:
-        answer_path = f'../data/all_answers_{llm_answers}.csv'
-        answers = pd.read_csv(answer_path)
+        try:
+            answer_path = f'../generate_llm_answers/answers/{llm_answers}/{prompt}.csv'
+            answers = pd.read_csv(answer_path)
+        except:
+            print(f'File not found: ../generate_llm_answers/answers/{llm_answers}/{prompt}.csv')
+            return
         answers = answers[answers['qid'] == int(qid)]
-        print(answers)
-        compatible_answers = pd.DataFrame({'docno': llm_answers + '_' + qid, 'text': answers['answer'], 'qid': qid, 'query': query})
+        colname = 'answer' + str(answer_number)
+        compatible_answers = pd.DataFrame({'docno': llm_answers + '_' + qid, 'text': answers[colname], 'qid': qid, 'query': query})
         passages_for_query = pd.concat([passages_for_query, compatible_answers], ignore_index=True)
 
     if model_name == 'monoT5':
         monoT5 = MonoT5ReRanker()
         res = monoT5.transform(passages_for_query)
-        res_path = './results/' + model_name + '/rankings/' + llm_answers + '/query_' + qid + '.csv'
-        if os.path.exists(res_path):
-            os.remove(res_path)
-
-        # create dir
-        if not os.path.exists('./results/' + model_name + '/rankings/' + llm_answers):
-            os.makedirs('./results/' + model_name + '/rankings/' + llm_answers)
+        
+        res_path = output_path + '/query_' + qid + '_rankings.csv'
         # sort by rank
         res = res.sort_values(by=['rank'])
         res.to_csv(res_path, index=False)
@@ -80,13 +87,8 @@ def main(qid, model_name, llm_answers):
         result_df = pd.DataFrame({'judgement': name, 'qid': qid, 'query': query, 'ndcg@10': ndcg, 'map': exp['map'][0], 'bpref': exp['bpref'][0], 'name': model_name, 'num_docs': num_docs, 'num_results': num_results}, index=[0])
         retrieval_results = pd.concat([retrieval_results, result_df], ignore_index=True)
     
-    output_path = './results/' + model_name + f'/{llm_answers}/query_' + qid + '.csv'
-    if os.path.exists(output_path):
-        os.remove(output_path)
-    # create dir
-    if not os.path.exists('./results/' + model_name + f'/{llm_answers}'):
-        os.makedirs('./results/' + model_name + f'/{llm_answers}')
-    retrieval_results.to_csv(output_path, index=False)
+    csv_path = output_path + '/query_' + qid + '_scores.csv'
+    retrieval_results.to_csv(csv_path, index=False)
     return 
 
 
@@ -100,5 +102,7 @@ if __name__ == '__main__':
                         chatgpt, falcon7b_instruct, falcon7b_prompt, falcon40b_instruct, falcon40b_prompt, OA_LLama
                         """
                         )
+    parser.add_argument('prompt', type=str, help='the name of the prompt to use. Possible values: q, question, multimedqa, no_prompt')
+    parser.add_argument('answer_number', type=int, help='the number of answers to use')
     args = parser.parse_args()
-    main(args.qid, args.model_name, args.llm_answers)
+    main(args.qid, args.model_name, args.llm_answers, args.prompt, args.answer_number)
--- indexing/querywise_indexing_execution_script.sh
+++ indexing/querywise_indexing_execution_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # list of all possible models
-all_llms="chatgpt falcon7b_instruct falcon7b_prompt falcon40b_instruct falcon40b_prompt OA_LLama"
+all_prompts=("q" "question" "multimedqa" "no_prompt")
+
+prompts=""
 llm_names=""
 quiet=false
 # parse command line arguments
@@ -17,6 +19,16 @@ while [[ $# -gt 0 ]]; do
         shift
         shift
         ;;
+        -p|--prompts)
+        prompts="$2"
+        shift
+        shift
+        ;;
+        -n|--num_answers)
+        num_answers="$2"
+        shift
+        shift
+        ;;
         -q|--quiet)
         quiet=true
         shift
@@ -26,6 +38,8 @@ while [[ $# -gt 0 ]]; do
         echo "Options:"
         echo "  -m, --model MODEL_NAME    Name of the model to use"
         echo "  -l, --llm LLM_NAMES      Names of the LLMs to use. If 'all' is specified, then all LLMs will be used"
+        echo "  -p, --prompts PROMPT_NAME  Name of the prompts to use. Options are q, question,multimedqa, no_prompt or all. Only used when llm is specified"
+        echo "  -n, --num_answers NUM_ANSWERS  Number of answers to index per topic. Only used when llm is specified"
         echo "  -q, --quiet              Run script in quiet mode"
         echo "  -h, --help               Show this help message and exit"
         exit 0
@@ -37,9 +51,38 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+# all_llms is a list of all possible LLMs, which are all directory names in ../generate_llm_answers/answers
+all_llms=$(ls ../generate_llm_answers/answers)
 # if llm_names = all, then set llm_names to all_llms
 if [ "$llm_names" = "all" ]; then
     llm_names="$all_llms"
+fi
+
+echo "LLMs to process: $llm_names"
+
+# if llm_names is not empty, then check that all llm_names are directories in ../generate_llm_answers/answers
+if [ ! -z "$llm_names" ]; then
+    for llm_name in $llm_names; do
+        if [ ! -d "../generate_llm_answers/answers/$llm_name" ]; then
+            echo "Invalid LLM name: $llm_name"
+            exit 1
+        fi
+    done
+fi
+
+# Check for prompts. If 'all' is specified, or if no prompts are specified, then set prompts to all_prompts
+if [ "$prompts" = "all" ] || [ -z "$prompts" ]; then
+    prompts="$all_prompts"
+fi
+
+# Check if prompts is list. If not, make it a list
+if [ ! -z "$prompts" ]; then
+    if [ ! -z "$prompts" ]; then
+        if [[ ! " ${all_prompts[@]} " =~ " ${prompts} " ]]; then
+            echo "Invalid prompt name: $prompts"
+            exit 1
+        fi
+    fi
 fi
 
 # load topics.txt file
@@ -68,13 +111,25 @@ while IFS= read -r line; do
                 python "$script_name" "$qid" "$model_name" > /dev/null 2>&1
             fi
         else
-            for llm_name in $llm_names; do
-                echo "LLM: $llm_name for Query ID: $qid"
-                if [ "$quiet" = false ]; then
-                    python "$script_name" "$qid" "$model_name" "$llm_name"
-                else
-                    python "$script_name" "$qid" "$model_name" "$llm_name" > /dev/null 2>&1
-                fi
+            for prompt in $prompts; do
+                for llm_name in $llm_names; do
+
+                    echo "LLM: $llm_name for Query ID: $qid"
+                    # loop over numbers from 1 to num_answers
+                    for i in $(seq 1 $num_answers); do 
+                        # check if file './results/' + model_name + f'/{llm_answers}/{prompt}/{str(answer_number)}/query_{qid}_scores.csv exists
+                        # if it does, then skip
+                        if [ -f "./results/$model_name/$llm_name/$prompt/$i/query_${qid}_scores.csv" ]; then
+                            echo "File exists: ./results/$model_name/$llm_name/$prompt/$i/query_${qid}_scores.csv"
+                            continue
+                        fi
+                        if [ "$quiet" = false ]; then
+                            python "$script_name" "$qid" "$model_name" "$llm_name" "$prompt" "$i"
+                        else
+                            python "$script_name" "$qid" "$model_name" "$llm_name" "$prompt" "$i" > /dev/null 2>&1
+                        fi
+                    done
+                done
             done
         fi
     fi
